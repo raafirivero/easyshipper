d6944d5bfc95d0d9f469023359d0f4a428c3e951
Raafi Rivero <raafi.rivero@gmail.com>
2014-01-05 18:00:12 -0500

    renamed the plugin EasyShipper Customs to keep wp happy

--- easypost.php
+++ easypost.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: EasyShipper
+Plugin Name: EasyShipper Customs
 Plugin URI: http://seanvoss.com/easypost
 Description: Provides an integration for EasyPost for woo-commerece.
 Version: 0.2.1
